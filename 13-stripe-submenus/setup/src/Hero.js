59367b3f8daf0f47d42a4364e6deff7039975f02
Geca13 <karafiloski84@gmail.com>
2021-04-21 16:59:10 +0200

    update

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
-
+import {useGlobalContext} from './context'
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const {closeSubmenu} = useGlobalContext()
+  return (
+  <section className='hero'>
+    <div className='hero-center'>
+      <article className='hero-info'>
+        <h1>Payment infastructure for the internet</h1>
+        <p>Millions .... People .... Spend.... Money.... to learn .... React.....</p>
+         <button className='btn'>Start now</button>
+      </article>
+      <article className='hero-images'>
+        <img src={phoneImg} className='phone-img' alt='phone'/>
+      </article>
+    </div>
+  </section>
+)}
 
 export default Hero
